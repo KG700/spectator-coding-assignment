668e3376482c3765df8978b343f493730b6e4826
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-02 16:15:47 +0100

    Added styling to articles to reduce margins between author, title and source

--- src/components/Articles/ArticleListItem/ArticleListItem.js
+++ src/components/Articles/ArticleListItem/ArticleListItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import classes from './ArticleListItem.module.css'
+
 const ArticleListItem = (props) => {
   return(
-    <li>
+    <li className={classes.ArticleListItem}>
       <h3>{props.author}</h3>
       <h2>{props.title}</h2>
       <p>source: {props.source}</p>
