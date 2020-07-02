812b2deb62daa69ec58b3745ab33e9a7086bb524
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-02 11:47:28 +0100

    Set up connection to NewsAPI to return 20 top UK headlines

--- src/components/Articles/ArticleCard/ArticleCard.js
+++ src/components/Articles/ArticleCard/ArticleCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ArticleCard = (props) => {
-
+  return null
 }
 
 export default ArticleCard;
--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -7,11 +7,27 @@ class Articles extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles = [];
+      articles: []
     }
   }
 
-  
+  componentDidMount() {
+    const url = 'http://newsapi.org/v2/top-headlines?' +
+                'country=gb&' +
+                'apiKey=' + process.env.REACT_APP_API_KEY;
+    const request = new Request(url);
+    fetch(request)
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error("Response wasn't ok");
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(() => console.log("Can't access " + url + "response."))
+  }
 
   render() {
 
